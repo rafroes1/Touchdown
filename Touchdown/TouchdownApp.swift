5ddc2c0f4dd05b9ee3b75c3f570ec3c8e2b90836
rafroes1 <rafafroes@outlook.com>
2023-02-14 10:24:35 -0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Rafael Carvalho on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
